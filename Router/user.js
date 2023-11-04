430ca81bf3305d2fbdd7e76f676b822ec8bd60eb
Faiz Naeem <81919141+FaizNaeem@users.noreply.github.com>
2023-11-04 18:34:22 +0500

    Add files via upload

--- Router/user.js
+++ Router/user.js
@@ -1,25 +1,54 @@
 const app = require("express")
 const router = app.Router()
-const user = require('../schema/user')
-router.post("/",(req , res)=>{
+const schemauser = require('../schema/user');
+const sendResponse = require("../helper/responce");
+router.post("/", async(req , res)=>{
     console.log("body console->>", req.body);
-res.send("post")
+try{
+const user = await schemauser.create({...req.body})
+if(user){
+    sendResponse(res ,200 , user, 'Post Success', false)
+}
+}
+catch(err){
+    sendResponse(res ,400 , user, 'Post not Sucess', true)
+}
 })
-router.put("/:id",(req , res)=>{
-    console.log("body console->>", req.body);
-    res.send("post")
+router.put("/:id",async(req , res)=>{
+    try{
+        const user = await schemauser.findByIdAndUpdate(req.params.id,{...req.body},{new :true}) 
+        if(user){
+            sendResponse(res ,200 , user, 'Edit Success', false)
+        }
+        }
+        catch(err){
+            sendResponse(res ,400 , user, 'Edit not Sucess', true)
+        }
     })
 
-    router.patch("/",(req , res)=>{
-        console.log("body console->>", req.body);
-        res.send("patch")
+    
+        router.get("/", async(req , res)=>{
+            try{
+                const users = await schemauser.find()
+                if(users){
+                    sendResponse(res ,200 , users, 'Find Success', false)
+                }
+                else{
+                    sendResponse(res ,400 , users, 'Find not Sucess', true)
+                }
+            }
+            catch(err){
+                sendResponse(res ,400 , user, 'Find not Sucess', true)
+            }
         })
-        router.get("/",(req , res)=>{
-            console.log("body console->>", req.body);
-            res.send("get")
-            })
-            router.delete("/:id",(req , res)=>{
-                console.log("body console->>", req.params.id);
-                res.send("delete")
+            router.delete("/:id",async (req , res)=>{
+                try{
+                    const users = await schemauser.findByIdAndDelete(req.params.id)
+                    sendResponse(res ,200 , users, 'Delete Success', false)
+                    
+                }
+                catch(err){
+                    sendResponse(res ,400 , user, 'Delete not Sucess', true)
+                }
                 })
                 module.exports=router
